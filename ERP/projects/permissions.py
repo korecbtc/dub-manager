a79484adb42da937bd01ba5cd6236ccf10ffa510
korecbtc <99734892+korecbtc@users.noreply.github.com>
2023-02-19 20:59:09 +0300

    test started

--- ERP/projects/permissions.py
+++ ERP/projects/permissions.py
@@ -4,24 +4,24 @@ from rest_framework import permissions
 class ManagerOrReadAndPatchOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in ('GET', 'PATCH')
+        return (request.user.is_authenticated
+                and (request.method in ('GET', 'PATCH')
                 or request.user.is_manager
-                or request.user.is_admin
+                or request.user.is_admin)
             )
 
 
 class ManagerOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method == 'GET'
+        return (request.user.is_authenticated
+                and (request.method == 'GET'
                 or request.user.is_manager
-                or request.user.is_admin
+                or request.user.is_admin)
             )
 
 
 class AdminOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.is_admin
+        return request.user.is_authenticated and request.user.is_admin
