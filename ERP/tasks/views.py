ca6ce840f2fffffe32dd9467eaeef98aa281ad5f
korecbtc <99734892+korecbtc@users.noreply.github.com>
2023-02-26 16:32:07 +0300

    +autodelete

--- ERP/tasks/views.py
+++ ERP/tasks/views.py
@@ -4,6 +4,8 @@ from . serializers import TaskSerializer, TaskCreateSerializer
 from . serializers import TaskAdminSerializer
 from projects.permissions import ManagerOrReadAndPatchOnly
 from django_filters.rest_framework import DjangoFilterBackend
+import datetime
+from django.utils import timezone
 
 
 class TaskViewset(viewsets.ModelViewSet):
@@ -11,6 +13,7 @@ class TaskViewset(viewsets.ModelViewSet):
     permission_classes = (ManagerOrReadAndPatchOnly, )
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'urgency', 'type']
+    PERIOD_FOR_DELETE = timezone.now() - datetime.timedelta(days=7)
 
     def get_serializer_class(self):
         if self.request.user.is_authenticated and self.request.user.is_manager:
@@ -18,3 +21,10 @@ class TaskViewset(viewsets.ModelViewSet):
         elif self.request.user.is_authenticated and self.request.user.is_admin:
             return TaskAdminSerializer
         return TaskSerializer
+
+    def perform_create(self, serializer):
+        '''Автоматическое удаление старых завершенных задач'''
+        Task.objects.filter(
+            time_create__lt=self.PERIOD_FOR_DELETE, status='finished'
+            ).delete()
+        return super().perform_create(serializer)
